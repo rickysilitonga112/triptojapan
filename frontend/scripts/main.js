03cf7d622f12b4b0ce1464afe469a47e91d28bf7
rickysilitonga112 <rickysilitonga112@gmail.com>
2021-05-26 21:27:18 +0700

    menyelesaikan detail page

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
   console.log("ready!");
+
+  $("#main_image, .xzoom-gallery").xzoom({
+    zoomWidth: 550,
+    title: false,
+    tint: "#333",
+    xoffset: 15,
+  });
 });
