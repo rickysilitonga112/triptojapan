4d8e64db9741cd232d60885b6ba0b5208a39fab3
rickysilitonga112 <rickysilitonga112@gmail.com>
2021-05-28 21:43:33 +0700

    membuat smooth scrooling pada website

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,10 +1,27 @@
 $(document).ready(function () {
-  console.log("ready!");
-
+  console.log("ready");
   $("#main_image, .xzoom-gallery").xzoom({
     zoomWidth: 550,
     title: false,
     tint: "#333",
     xoffset: 15,
+  });
+
+  // smooth scrolling
+  $(".page-scroll").on("click", function (e) {
+    var href = $(this).attr("href");
+    var elemenTujuan = $(href);
+    console.log(elemenTujuan.offset().top);
+
+    // pindahkan scroll
+    $("html, body").animate(
+      {
+        scrollTop: elemenTujuan.offset().top - 120,
+      },
+      800,
+      "easeInOutExpo"
+    );
+
+    e.preventDefault();
   });
 });
